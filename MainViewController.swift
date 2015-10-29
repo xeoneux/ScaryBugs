7a9bef032a5ae011f30e0a4f22b17e2f9b49ae5f
Aayush Kapoor <xeoneux@icloud.com>
2015-10-30 04:06:23 +0530

    Add MainViewController

---
+++ MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ScaryBugs
+//
+//  Created by Aayush Kapoor on 30/10/15.
+//  Copyright © 2015 Aayush Kapoor. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
