6ef575cb88f0ef8adf737f140fa72d3e448c5528
Aayush Kapoor <xeoneux@icloud.com>
2015-10-30 04:22:42 +0530

    Add App and Bug Icons with Data

--- ScaryBugs/MainViewController.swift
+++ ScaryBugs/MainViewController.swift
@@ -9,10 +9,25 @@
 import Cocoa
 
 class MainViewController: NSViewController {
+    
+    var bugs: [ScaryBugsDoc]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    func setupSampleBugs() {
+        let bug1 = ScaryBugsDoc(title: "Potato Bug", rating: 4.0,
+            thumbImage:NSImage(named: "potatoBugThumb"), fullImage: NSImage(named: "potatoBug"))
+        let bug2 = ScaryBugsDoc(title: "House Centipede", rating: 3.0,
+            thumbImage:NSImage(named: "centipedeThumb"), fullImage: NSImage(named: "centipede"))
+        let bug3 = ScaryBugsDoc(title: "Wolf Spider", rating: 5.0,
+            thumbImage:NSImage(named: "wolfSpiderThumb"), fullImage: NSImage(named: "wolfSpider"))
+        let bug4 = ScaryBugsDoc(title: "Lady Bug", rating: 1.0,
+            thumbImage:NSImage(named: "ladybugThumb"), fullImage: NSImage(named: "ladybug"))
+        
+        bugs = [bug1, bug2, bug3, bug4]
     }
     
 }
